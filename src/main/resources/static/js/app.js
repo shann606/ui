179ef7dd692b570ce2836bc5e4e350413ac0a211
shann606 <shann606@gmail.com>
2026-09-04 11:23:38 +0530

    Added new files

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,6 +1,22 @@
 window.addEventListener('load', async () => {
+
+
+    const userId = document.getElementById("userId").value;
+
+    const month = new Date().toLocaleString('en-US', {
+        month: 'long'
+    });
+
+    const title = document.getElementById("expense");
+
+    title.innerHTML = `Total expense of ${month}`;
+
+
     try {
-        const response = await fetch('/api/v1/expenses');
+        let totalExpense = 0;
+        const response = await fetch('/api/v1/expenses/dashboard/' + userId);
+
+
 
         if (!response.ok) {
             throw new Error('Failed to fetch expenses');
@@ -8,23 +24,38 @@ window.addEventListener('load', async () => {
 
         const expenses = await response.json();
 
+        console.log(JSON.stringify(expenses));
+
         const tbody = document.getElementById('expenseTableBody');
-        let totalExpense = 0;
 
-        expenses.forEach(expense => {
-            totalExpense += expense.amount;
 
-            const row = `
-                <tr>
-                    <td><a href="#">${expense.id} </a></td> 
-                    <td>${expense.category}</td>
-                    <td>${expense.amount}</td>
-                    <td>${expense.date}</td>
-                </tr>
-            `;
+        Object.keys(expenses).forEach((key) => {
+            totalExpense = key;
+            console.log(key);
 
-            tbody.insertAdjacentHTML('beforeend', row);
+            expenses[key].forEach((value) => {
+
+
+                const row = `
+			              <tr>
+			                  <td><a href="/expenses/search">${value.categoryname} </a></td> 
+			                  <td>${value.subCategoryName}</td>
+			                  <td>${value.amount}</td>
+							  <td>${value.payment}</td>
+			                  <td>${new Date(value.spenton).toLocaleDateString("en-GB")}</td>
+			              </tr>
+			          `;
+
+                tbody.insertAdjacentHTML('beforeend', row);
+
+
+
+            });
         });
+
+
+
+
 
         document.getElementById('totalexpense').textContent = totalExpense;
 
